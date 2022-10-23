f8fb19362b18fe34bd66ec3544336bc44766b6c0
High <Justla1993@gmail.com>
2022-10-23 23:05:33 +0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,13 @@
+let x = {};
+if (typeof x === 'number') {
+  console.log('Это число')
+}
+else if(typeof x === 'string') {
+  console.log('Это строка')
+}
+else if(typeof x === 'boolean') {
+  console.log('Это логический тип')
+}
+else {
+  console.log('Тип x не определён')
+}
